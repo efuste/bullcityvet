db4c8363016c3f22628571b714bd306ca0260a2e
efuste <Erik@Eriks-MacBook-Air.local>
2013-10-25 23:43:19 -0400

    Update Files
    update layout and index.html

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+$(function() {
+	$(document).on('focusin', '.field, textarea', function() {
+		if(this.title==this.value) {
+			this.value = '';
+		}
+	}).on('focusout', '.field, textarea', function(){
+		if(this.value=='') {
+			this.value = this.title;
+		}
+	});
+
+	$(".slider ul").carouFredSel({
+		auto: 3500
+	});
+
+	$(document).on('mouseenter', '.navigation ul li', function () {
+		$(this).addClass('current').find('> ul').stop(true,true).slideDown(200);
+	}).on('mouseleave', '.navigation ul li', function () {
+		$(this).removeClass('current').find('> ul').stop(true,true).slideUp(200);
+	});
+
+
+});
