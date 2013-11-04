df704bf666c9b6bfd8d439f330c665b575558a1a
Erik Fuste <erik.fuste@pxl83.com>
2013-11-03 22:21:03 -0500

    Update
    updates

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php include('includes/header.php'); ?>
+
+		<div class="container">
+			<div class="slider">
+				<ul>
+					<li>
+						<img src="css/images/slide-1.jpg" alt="" />
+						<div class="slide-text"><h2><a href="#">Coming Soon,<br /> Bull City Animal Hospital</a></h2></div>
+					</li>
+
+					
+				</ul>
+			</div><!-- /.slider -->
+
+			<div class="main home">
+				<div class="content">
+					<div class="entry">
+						<h2>Welcome to Bull City Veterinary Hospital </h2>
+
+						<p>We are proud to introduce the 1st Veterinary Hospital to the Downtown Durham area.Our goal is to provide excellent veterinary care and exceptional customer service to the local community. Our state of the art equipment and trained staff will facilitate our  goal. We emphasize the importance of continuing education  to stay up to date with the latest studies and technology to be able to offer the best possible care to our patients . We are very excited to serve the local community  and to be a part of local upcoming events.</p>
+					</div><!-- /.entry -->
+
+					<div class="boxes clearfix">
+						<div class="box box1 service">
+							<h3>Service one</h3>
+
+							<div class="box-content">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum aliquet varius. Maecenas mattis rutrum condimentum. Pellentesque at nisi </p>
+							</div><!-- /.box-content -->
+							
+							<p class="buttons"><a href="#" class="more">Learn More &gt;</a></p><!-- /.buttons -->
+						</div><!-- /.box box1 -->
+
+						<div class="box box1 service">
+							<h3>Service two</h3>
+
+							<div class="box-content">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum aliquet varius. Maecenas mattis rutrum condimentum. Pellentesque at nisi </p>
+							</div><!-- /.box-content -->
+							
+							<p class="buttons"><a href="#" class="more">Learn More &gt;</a></p><!-- /.buttons -->
+						</div><!-- /.box box1 -->
+
+						<div class="box box1 service">
+							<h3>Service three</h3>
+
+							<div class="box-content">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum aliquet varius. Maecenas mattis rutrum condimentum. Pellentesque at nisi </p>
+							</div><!-- /.box-content -->
+							
+							<p class="buttons"><a href="#" class="more">Learn More &gt;</a></p><!-- /.buttons -->
+						</div><!-- /.box box1 -->
+					</div><!-- /.boxes -->
+
+					<div class="content-bottom clearfix">
+						<div class="newsletter-form">
+							<h4>Newsletter Signup</h4>
+							<form action="" method="post">
+								<div class="row clearfix"><input type="text" title="Email Address" value="Email Address" class="field" /></div><!-- /.row -->	
+								<div class="row clearfix"><input type="submit" value="Sign" class="submit" /></div>
+							</form>
+						</div><!-- /.newsletter-form -->
+
+						<div class="quotes">
+							<h2>Testimonials</h2>
+							<div class="quote">
+								<blockquote>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec lacus quam. Sed ornare, augue eget aliquet vehicula, purus libero elementum neque, vel hendrerit eros lacus non leo. Morbi in elit quam, quis porta justo. Vivamus quis lectus nec quam ultricies sollicitudin vel at quam. Nulla sed ornare dui. Etiam tempus sollicitudin massa, eu tincidunt libero 
+								</blockquote>
+							</div><!-- /.quote -->
+						</div><!-- /.quotes -->
+					</div><!-- /.content-bottom -->
+				</div><!-- /.content -->
+			</div><!-- /.main -->
+		</div><!-- /.container -->
+
+<?php include('includes/footer.php'); ?>
+		
