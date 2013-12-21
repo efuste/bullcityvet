96592fa4bfd7b96d772920b223c7ace5c275edee
Erik Fuste <erik.fuste@pxl83.com>
2013-12-21 16:38:29 -0500

    Revert "Merge branch 'master' of https://github.com/efuste/bullcityvet"
    This reverts commit c376228c289947c6d91f4b0a757639ee5f7bdc85, reversing
    changes made to d1cc61a80465662b25b3c36294721ac3d952e19a.

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <?php include('header.php'); ?>
-=======
-<?php include('includes/header.php'); ?>
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
 
 		<div class="container">
 			<div class="slider">
@@ -12,7 +8,6 @@
 						<div class="slide-text"><h2><a href="#">Coming Soon,<br /> Bull City Animal Hospital</a></h2></div>
 					</li>
 
-<<<<<<< HEAD
 					<li>
 						<img src="css/images/slide-1.jpg" alt="" />
 						<div class="slide-text"><h2><a href="#">Coming Soon,<br /> Bull City Animal Hospital</a></h2></div>
@@ -22,16 +17,12 @@
 						<img src="css/images/slide-1.jpg" alt="" />
 						<div class="slide-text"><h2><a href="#">Coming Soon,<br /> Bull City Animal Hospital</a></h2></div>
 					</li>
-=======
-					
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
 				</ul>
 			</div><!-- /.slider -->
 
 			<div class="main home">
 				<div class="content">
 					<div class="entry">
-<<<<<<< HEAD
 						<h2>Welcome to Bull City Veterinary Hospital .</h2>
 
 						<p>
@@ -42,16 +33,10 @@
 						    best possible care to our patients .
 							We are very excited to serve the local community  and to be a part of local upcoming events.
 						</p>
-=======
-						<h2>Welcome to Bull City Veterinary Hospital </h2>
-
-						<p>We are proud to introduce the 1st Veterinary Hospital to the Downtown Durham area.Our goal is to provide excellent veterinary care and exceptional customer service to the local community. Our state of the art equipment and trained staff will facilitate our  goal. We emphasize the importance of continuing education  to stay up to date with the latest studies and technology to be able to offer the best possible care to our patients . We are very excited to serve the local community  and to be a part of local upcoming events.</p>
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
 					</div><!-- /.entry -->
 
 					<div class="boxes clearfix">
 						<div class="box box1 service">
-<<<<<<< HEAD
 							<h3>Acupuncture</h3>
 
 							<div class="box-content">
@@ -61,19 +46,12 @@
 									diseases or health issues or it can be used as a complementary form of treatment with other 
 									types of medicine.
 								</p>
-=======
-							<h3>Service one</h3>
-
-							<div class="box-content">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum aliquet varius. Maecenas mattis rutrum condimentum. Pellentesque at nisi </p>
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
 							</div><!-- /.box-content -->
 							
 							<p class="buttons"><a href="#" class="more">Learn More &gt;</a></p><!-- /.buttons -->
 						</div><!-- /.box box1 -->
 
 						<div class="box box1 service">
-<<<<<<< HEAD
 							<h3>Tui Na</h3>
 
 							<div class="box-content">
@@ -81,19 +59,12 @@
 									acupuncture to achieve balance in a patient. It has some similarities to chiropractic work and 
 									massage therapy, but it's general purpose is to resolve disease states.
 								</p>
-=======
-							<h3>Service two</h3>
-
-							<div class="box-content">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum aliquet varius. Maecenas mattis rutrum condimentum. Pellentesque at nisi </p>
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
 							</div><!-- /.box-content -->
 							
 							<p class="buttons"><a href="#" class="more">Learn More &gt;</a></p><!-- /.buttons -->
 						</div><!-- /.box box1 -->
 
 						<div class="box box1 service">
-<<<<<<< HEAD
 							<h3>Therapeutic Laser</h3>
 
 							<div class="box-content">
@@ -102,12 +73,6 @@
 									the alleviation of the pain of arthritis, improving circulation, promoting wound and incision 
 									healing and decreasing inﬂammation.
 								</p>
-=======
-							<h3>Service three</h3>
-
-							<div class="box-content">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum aliquet varius. Maecenas mattis rutrum condimentum. Pellentesque at nisi </p>
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
 							</div><!-- /.box-content -->
 							
 							<p class="buttons"><a href="#" class="more">Learn More &gt;</a></p><!-- /.buttons -->
@@ -117,7 +82,6 @@
 					<div class="content-bottom clearfix">
 						<div class="newsletter-form">
 							<h4>Newsletter Signup</h4>
-<<<<<<< HEAD
 							 <form action="formsubmit.php" method="post">
             					<fieldset id="contact">
               					<input name="your_name" type="text" class="forms" id="your_name" size="35" value="Name"/>
@@ -129,19 +93,11 @@
             					</fieldset>
           					</form>
 							</div><!-- /.newsletter-form -->
-=======
-							<form action="" method="post">
-								<div class="row clearfix"><input type="text" title="Email Address" value="Email Address" class="field" /></div><!-- /.row -->	
-								<div class="row clearfix"><input type="submit" value="Sign" class="submit" /></div>
-							</form>
-						</div><!-- /.newsletter-form -->
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
 
 						<div class="quotes">
 							<h2>Testimonials</h2>
 							<div class="quote">
 								<blockquote>
-<<<<<<< HEAD
 									Dr. Sabin is awesome! She has greatly enhanced the quality of life for CoCo, our 16 year-old Chow/
 									German Shepherd mix, who has really bad arthritis in her hips and back legs. Dr. Sabin has provided 
 									mobile veterinary care for CoCo in our home for over 2 years. She alternates acupuncture treatments 
@@ -151,9 +107,6 @@
 									compassionate human being! We feel very fortunate to have such a great combination of qualities in 
 									one individual to provide care for our beloved CoCo. I would highly recommend Dr. Sabin to anyone who 
 									demands the best of care possible for the furry members of their family!!
-=======
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec lacus quam. Sed ornare, augue eget aliquet vehicula, purus libero elementum neque, vel hendrerit eros lacus non leo. Morbi in elit quam, quis porta justo. Vivamus quis lectus nec quam ultricies sollicitudin vel at quam. Nulla sed ornare dui. Etiam tempus sollicitudin massa, eu tincidunt libero 
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
 								</blockquote>
 							</div><!-- /.quote -->
 						</div><!-- /.quotes -->
@@ -162,9 +115,4 @@
 			</div><!-- /.main -->
 		</div><!-- /.container -->
 
-<<<<<<< HEAD
 <?php include('footer.php'); ?>
-=======
-<?php include('includes/footer.php'); ?>
-		
->>>>>>> f706aed05f89ec6e7875fc52a64158830c845829
